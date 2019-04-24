7859a3a0e0919f73acfe4b973907e89cd1f2ccd5
Daven Todd <dt02553@georgiasouthern.edu>
2019-04-24 10:32:48 -0400

    added leaderboard activity and content provider for scores

--- app/src/main/java/com/example/quadrominotest/LeaderboardActivity.java
+++ app/src/main/java/com/example/quadrominotest/LeaderboardActivity.java
@@ -1,4 +1,106 @@
-package com.example.quadrominotest;
+package com.example.quadraminotest;
 
-public class LeaderboardActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LeaderboardActivity extends Activity {
+
+    ListView list;
+
+    void PopulateListView() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        ArrayList<HashMap<String, String>> mList = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> mMap;
+
+        String[] projection = {
+                LeaderboardDB.KEY_NAME,
+                LeaderboardDB.KEY_SCORE};
+
+        String selection = LeaderboardDB.KEY_DIFFICULTY + "=?" + " and "
+                + LeaderboardDB.KEY_NUMROWS + "=?" + " and "
+                + LeaderboardDB.KEY_NUMCOLUMNS + "=?" + " and "
+                + LeaderboardDB.KEY_SPEED + " =?";
+
+        String[] selectionArgs = {
+                prefs.getString("difficulty_preference_highscores", "Normal"),
+                prefs.getString("num_rows_preference_highscores", "20"),
+                prefs.getString("num_columns_preference_highscores", "10"),
+                prefs.getString("speed_preference_highscores", "Normal")
+        };
+
+        String sortOrder = LeaderboardDB.KEY_SCORE + " DESC";
+
+        Cursor cursor = getContentResolver().query(LeaderboardContentProvider.CONTENT_URI, projection, selection, selectionArgs, sortOrder);
+
+        if (cursor == null || cursor.getCount() == 0) {
+            mMap = new HashMap<String, String>();
+            mMap.put("ID", "1");
+            mMap.put("Name", "N/A");
+            mMap.put("Score", "N/A");
+            mList.add(mMap);
+        } else {
+            int id = 0;
+            cursor.moveToFirst();
+            do {
+                id++;
+                mMap = new HashMap<String, String>();
+                mMap.put("ID", Integer.toString(id));
+                mMap.put("Name", cursor.getString(cursor.getColumnIndexOrThrow(LeaderboardDB.KEY_NAME)));
+                mMap.put("Score", Integer.toString(cursor.getInt(cursor.getColumnIndexOrThrow(LeaderboardDB.KEY_SCORE))));
+                mList.add(mMap);
+            }
+            while (cursor.moveToNext());
+        }
+
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this, mList, R.layout.rows_leaderboard, new String[]{"ID", "Name", "Score"}, new int[]{R.id.ID_CELL, R.id.NAME_CELL, R.id.SCORE_CELL});
+        list.setAdapter(mSimpleAdapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+
+        list = (ListView) findViewById(R.id.listView_highscores);
+        Button setPropertiesButton = (Button) findViewById(R.id.set_properties_highscores_button);
+        setPropertiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LeaderboardActivity.this.startActivity(new Intent(LeaderboardActivity.this, LeaderboardSettingsActivity.class));
+            }
+        });
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (SaveSettings.saveSettingsHighscores == 0) {
+            SaveSettings.saveSettingsHighscores = 1;
+            prefs.edit().putString("difficulty_preference_highscores", "Normal").apply();
+            prefs.edit().putString("num_rows_preference_highscores", "20").apply();
+            prefs.edit().putString("num_columns_preference_highscores", "10").apply();
+            prefs.edit().putString("speed_preference_highscores", "Normal").apply();
+        }
+        PopulateListView();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        PopulateListView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PopulateListView();
+    }
 }
--- app/src/main/java/com/example/quadrominotest/LeaderboardContentProvider.java
+++ app/src/main/java/com/example/quadrominotest/LeaderboardContentProvider.java
@@ -1,4 +1,58 @@
-package com.example.quadrominotest;
+package com.example.quadraminotest;
 
-public class LeaderboardContentProvider {
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+public class LeaderboardContentProvider extends ContentProvider {
+
+    private LeaderboardDBHelper dbHelper;
+
+    private static final String AUTHORITY = "com.indysoft.amazingtetris.contentprovider";
+
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://" + AUTHORITY + "/highscores");
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new LeaderboardDBHelper(getContext());
+        return false;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        long id = db.insert(LeaderboardDB.SQLITE_TABLE, null, values);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return Uri.parse(CONTENT_URI + "/" + id);
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(LeaderboardDB.SQLITE_TABLE);
+
+        Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
 }
