3cbfa5c98064b99b6ac1bb3558b42dde7ca07132
Daven Todd <dt02553@georgiasouthern.edu>
2019-04-24 10:28:21 -0400

    drawing objects on gameboard

--- app/src/main/java/com/example/quadrominotest/GameActivity.java
+++ app/src/main/java/com/example/quadrominotest/GameActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -200,4 +201,268 @@ public class GameActivity {
             }
         }
     }
+
+    private boolean MoveShape(final int direction, Shape nowShape) {
+        BoardCell[][] aux = new BoardCell[NUM_ROWS][];
+        for (int i = 0; i < NUM_ROWS; ++i)
+            aux[i] = new BoardCell[NUM_COLUMNS];
+        CopyMatrix(gameMatrix, aux);
+        int i, ii, j, jj;
+
+        for (ii = nowShape.x, i = 1; i <= 4; ++i, ++ii) {
+            for (jj = nowShape.y, j = 1; j <= 4; ++j, ++jj) {
+                if (nowShape.mat[i][j].getState() == 1) {
+                    gameMatrix[ii][jj] = new BoardCell();
+                }
+            }
+        }
+
+        for (ii = nowShape.x + dx[direction], i = 1; i <= 4; ++i, ++ii) {
+            for (jj = nowShape.y + dy[direction], j = 1; j <= 4; ++j, ++jj) {
+                gameMatrix[ii][jj].setState(gameMatrix[ii][jj].getState() + nowShape.mat[i][j].getState());
+                if (nowShape.mat[i][j].getState() == 1) {
+                    gameMatrix[ii][jj].setColor(nowShape.mat[i][j].getColor());
+                    gameMatrix[ii][jj].setBehavior(nowShape.mat[i][j].getBehavior());
+                }
+                if (gameMatrix[ii][jj].getState() > 1) {
+                    CopyMatrix(aux, gameMatrix);
+                    FixGameMatrix();
+                    return false;
+                }
+            }
+        }
+        nowShape.x += dx[direction];
+        nowShape.y += dy[direction];
+        FixGameMatrix();
+        return true;
+    }
+
+    private boolean RotateLeft(Shape nowShape) {
+        BoardCell[][] aux = new BoardCell[NUM_ROWS][];
+        for (int i = 0; i < NUM_ROWS; ++i)
+            aux[i] = new BoardCell[NUM_COLUMNS];
+        CopyMatrix(gameMatrix, aux);
+        int i, ii, j, jj;
+
+        for (ii = nowShape.x, i = 1; i <= 4; ++i, ++ii) {
+            for (jj = nowShape.y, j = 1; j <= 4; ++j, ++jj) {
+                if (nowShape.mat[i][j].getState() == 1) {
+                    gameMatrix[ii][jj] = new BoardCell();
+                }
+            }
+        }
+        nowShape.RotateLeft();
+
+        for (ii = nowShape.x, i = 1; i <= 4; ++i, ++ii) {
+            for (jj = nowShape.y, j = 1; j <= 4; ++j, ++jj) {
+                gameMatrix[ii][jj].setState(gameMatrix[ii][jj].getState() + nowShape.mat[i][j].getState());
+                if (nowShape.mat[i][j].getState() == 1) {
+                    gameMatrix[ii][jj].setColor(nowShape.mat[i][j].getColor());
+                    gameMatrix[ii][jj].setBehavior(nowShape.mat[i][j].getBehavior());
+                }
+
+                if (gameMatrix[ii][jj].getState() > 1) {
+                    CopyMatrix(aux, gameMatrix);
+
+                    nowShape.RotateRight();
+                    FixGameMatrix();
+                    return false;
+                }
+            }
+        }
+        FixGameMatrix();
+        return true;
+    }
+
+    private boolean RotateRight(Shape nowShape) {
+        BoardCell[][] aux = new BoardCell[NUM_ROWS][];
+        for (int i = 0; i < NUM_ROWS; ++i)
+            aux[i] = new BoardCell[NUM_COLUMNS];
+        CopyMatrix(gameMatrix, aux);
+        int i, ii, j, jj;
+        for (ii = nowShape.x, i = 1; i <= 4; ++i, ++ii) {
+            for (jj = nowShape.y, j = 1; j <= 4; ++j, ++jj) {
+                if (nowShape.mat[i][j].getState() == 1) {
+                    gameMatrix[ii][jj] = new BoardCell();
+                }
+            }
+        }
+        nowShape.RotateRight();
+        for (ii = nowShape.x, i = 1; i <= 4; ++i, ++ii) {
+            for (jj = nowShape.y, j = 1; j <= 4; ++j, ++jj) {
+                gameMatrix[ii][jj].setState(gameMatrix[ii][jj].getState() + nowShape.mat[i][j].getState());
+                if (nowShape.mat[i][j].getState() == 1) {
+                    gameMatrix[ii][jj].setColor(nowShape.mat[i][j].getColor());
+                    gameMatrix[ii][jj].setBehavior(nowShape.mat[i][j].getBehavior());
+                }
+                if (gameMatrix[ii][jj].getState() > 1) {
+                    CopyMatrix(aux, gameMatrix);
+                    nowShape.RotateLeft();
+                    FixGameMatrix();
+                    return false;
+                }
+            }
+        }
+        FixGameMatrix();
+        return true;
+    }
+
+    private boolean CreateShape() {
+        if (difficulty.compareTo("Normal") == 0) {
+            currentShape = shapes[random.nextInt(7)];
+        } else {
+            currentShape = shapes[random.nextInt(shapes.length)];
+        }
+
+        int number_of_rotations = random.nextInt(4);
+        for (int i = 1; i <= number_of_rotations; ++i) {
+            currentShape.RotateRight();
+        }
+        currentShape.x = 0;
+        currentShape.y = 1 + (NUM_COLUMNS - 6) / 2;
+
+        for (int offset = 0; offset <= 3; ++offset) {
+            int i, ii, j, jj;
+            boolean ok = true;
+            for (ii = currentShape.x + offset, i = 1; i <= 4; ++i, ++ii) {
+                for (jj = currentShape.y, j = 1; j <= 4; ++j, ++jj) {
+                    gameMatrix[ii][jj].setState(gameMatrix[ii][jj].getState() + currentShape.mat[i][j].getState());
+                    if (gameMatrix[ii][jj].getState() > 1) {
+                        ok = false;
+                    }
+                }
+            }
+            if (ok) {
+                for (i = 1, ii = currentShape.x + offset; i <= 4; ++i, ++ii) {
+                    for (j = 1, jj = currentShape.y; j <= 4; ++j, ++jj) {
+                        if (currentShape.mat[i][j].getState() == 1) {
+                            gameMatrix[ii][jj].setColor(currentShape.mat[i][j].getColor());
+                            gameMatrix[ii][jj].setBehavior(currentShape.mat[i][j].getBehavior());
+                        }
+                    }
+                }
+                currentShape.x += offset;
+                FixGameMatrix();
+                return true;
+            } else {
+                for (ii = currentShape.x + offset, i = 1; i <= 4; ++i, ++ii) {
+                    for (jj = currentShape.y, j = 1; j <= 4; ++j, ++jj) {
+                        gameMatrix[ii][jj].setState(gameMatrix[ii][jj].getState() - currentShape.mat[i][j].getState());
+                    }
+                }
+            }
+        }
+        FixGameMatrix();
+        return false;
+    }
+
+    private boolean Check() {
+        int k = 0;
+        boolean found = false;
+        for (int i = NUM_ROWS - 4; i >= 3; --i) {
+            boolean ok = true;
+            for (int j = 3; j < NUM_COLUMNS - 3; ++j) {
+                if (gameMatrix[i][j].getState() == 0) {
+                    ok = false;
+                }
+            }
+            if (ok) {
+                ++k;
+                found = true;
+            } else {
+                if (k == 0)
+                    continue;
+                for (int j = 3; j < NUM_COLUMNS - 3; ++j) {
+                    int state = gameMatrix[i][j].getState();
+                    int color = gameMatrix[i][j].getColor();
+                    int behavior = gameMatrix[i][j].getBehavior();
+                    gameMatrix[i + k][j] = new BoardCell(state, color, behavior);
+                }
+            }
+        }
+        for (int pas = 0; pas < k; ++pas) {
+            for (int j = 3; j < NUM_COLUMNS - 3; ++j) {
+                gameMatrix[3 + pas][j] = new BoardCell();
+            }
+        }
+        score += k * (k + 1) / 2;
+        FixGameMatrix();
+        return found;
+    }
+
+    void PaintMatrix() {
+
+        paint.setColor(Color.BLACK);
+        canvas.drawRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT, paint);
+
+        paint.setColor(Color.WHITE);
+        for (int i = 0; i <= (NUM_ROWS - 6); ++i) {
+            canvas.drawLine(0, i * (BOARD_HEIGHT / (NUM_ROWS - 6)), BOARD_WIDTH,
+                    i * (BOARD_HEIGHT / (NUM_ROWS - 6)), paint);
+        }
+        for (int i = 0; i <= (NUM_COLUMNS - 6); ++i) {
+            canvas.drawLine(i * (BOARD_WIDTH / (NUM_COLUMNS - 6)), 0,
+                    i * (BOARD_WIDTH / (NUM_COLUMNS - 6)), BOARD_HEIGHT, paint);
+        }
+
+        for (int i = 3; i < NUM_ROWS - 3; ++i) {
+            for (int j = 3; j < NUM_COLUMNS - 3; ++j) {
+                if (gameMatrix[i][j].getState() == 1) {
+                    paint.setColor(gameMatrix[i][j].getColor());
+                    canvas.drawRect((j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            paint);
+                }
+            }
+        }
+
+        for (int i = 3; i < NUM_ROWS - 3; ++i) {
+            for (int j = 3; j < NUM_COLUMNS - 3; ++j) {
+                if (gameMatrix[i][j].getState() == 1) {
+                    paint.setColor(Color.BLACK);
+                    canvas.drawLine((j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            (j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            paint);
+                    canvas.drawLine((j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            paint);
+                    canvas.drawLine((j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            paint);
+                    canvas.drawLine((j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)),
+                            (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)),
+                            paint);
+                }
+            }
+        }
+
+        if (!gameInProgress) {
+            TextView textView = (TextView) findViewById(R.id.game_over_textview);
+            textView.setVisibility(View.VISIBLE);
+            TextView textView2 = (TextView) findViewById(R.id.game_over_textview2);
+            textView2.setVisibility(View.VISIBLE);
+        } else if (gamePaused) {
+            paint.setColor(Color.WHITE);
+            paint.setTextAlign(Paint.Align.CENTER);
+            paint.setTextSize(60);
+            canvas.drawText("GAME PAUSED", (float) (BOARD_WIDTH / 2.0), (float) (BOARD_HEIGHT / 2.0), paint);
+        }
+
+        linearLayout.setBackgroundDrawable(new BitmapDrawable(bitmap));
+
+        TextView textView = (TextView) findViewById(R.id.game_score_textview);
+        textView.setText("Score: " + score);
+    }
+
+
 }
