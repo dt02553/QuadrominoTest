9b64135c10d824020794e3882b1f15f675d9a8c6
Daven Todd <dt02553@georgiasouthern.edu>
2019-04-24 10:21:31 -0400

    initial gameboard work

--- app/src/main/java/com/example/quadrominotest/GameActivity.java
+++ app/src/main/java/com/example/quadrominotest/GameActivity.java
@@ -1,4 +1,63 @@
 package com.example.quadrominotest;
 
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 public class GameActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        difficulty = prefs.getString("difficulty_preference", "Normal");
+        NUM_ROWS = Integer.parseInt(prefs.getString("num_rows_preference", "20")) + 6;
+        NUM_COLUMNS = Integer.parseInt(prefs.getString("num_columns_preference", "10")) + 6;
+        speed = prefs.getString("speed_preference", "Normal");
+        switch (speed) {
+            case "Slow": {
+                SPEED_NORMAL = 1000;
+                SPEED_FAST = 100;
+                break;
+            }
+            case "Normal": {
+                SPEED_NORMAL = 500;
+                SPEED_FAST = 50;
+                break;
+            }
+            case "Fast": {
+                SPEED_NORMAL = 250;
+                SPEED_FAST = 25;
+                break;
+            }
+        }
+
+        TextView textView = (TextView) findViewById(R.id.game_over_textview);
+        textView.setVisibility(View.INVISIBLE);
+        TextView textView2 = (TextView) findViewById(R.id.game_over_textview2);
+        textView2.setVisibility(View.INVISIBLE);
+
+        bitmap = Bitmap.createBitmap(BOARD_WIDTH, BOARD_HEIGHT, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        paint = new Paint();
+        linearLayout = (LinearLayout) findViewById(R.id.game_board);
+        score = 0;
+        currentShapeAlive = false;
+
+        gestureDetector = new GestureDetectorCompat(this, this);
+        gestureDetector.setOnDoubleTapListener(this);
+
+        ShapesInit();
+
+        GameInit();
+    }
 }
--- app/src/main/java/com/example/quadrominotest/MainActivity.java
+++ app/src/main/java/com/example/quadrominotest/MainActivity.java
@@ -1,4 +1,67 @@
 package com.example.quadrominotest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.quadraminotest.R;
+
 public class MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (SaveSettings.saveSettingsApp == 0) {
+            SaveSettings.saveSettingsApp = 1;
+            prefs.edit().putString("difficulty_preference", "Normal").apply();
+            prefs.edit().putString("num_rows_preference", "20").apply();
+            prefs.edit().putString("num_columns_preference", "10").apply();
+            prefs.edit().putString("speed_preference", "Normal").apply();
+        }
+
+        SharedPreferences settings = getSharedPreferences("Preferences", 0);
+
+        final Button newGameButton = (Button) findViewById(R.id.new_game_button);
+
+        Button leaderboardButton = (Button) findViewById(R.id.leaderboard_button);
+        leaderboardButton.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        SaveSettings.saveSettingsLeaderboard = 0;
+                        MainActivity.this.startActivity(new Intent(MainActivity.this, LeaderboardActivity.class));
+                    }
+                });
+
+        Button infoButton = (Button) findViewById(R.id.info_button);
+        infoButton.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        MainActivity.this.startActivity(new Intent(MainActivity.this, InfoActivity.class));
+                    }
+                });
+
+        Button settingsButton = (Button) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        MainActivity.this.startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                    }
+                });
+
+    }
 }
