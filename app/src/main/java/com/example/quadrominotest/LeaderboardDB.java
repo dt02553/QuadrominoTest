b9e24ff6e7f131fb6f15f97945de66bed775e7de
Daven Todd <dt02553@georgiasouthern.edu>
2019-04-24 10:34:15 -0400

    created database and scripts

--- app/src/main/java/com/example/quadrominotest/LeaderboardDB.java
+++ app/src/main/java/com/example/quadrominotest/LeaderboardDB.java
@@ -1,4 +1,36 @@
 package com.example.quadrominotest;
 
+import android.database.sqlite.SQLiteDatabase;
+
 public class LeaderboardDB {
+
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_SCORE = "score";
+    public static final String KEY_DIFFICULTY = "difficulty";
+    public static final String KEY_NUMROWS = "numrows";
+    public static final String KEY_NUMCOLUMNS = "numcolumns";
+    public static final String KEY_SPEED = "speed";
+
+    public static final String SQLITE_TABLE = "LeaderboardTable";
+
+    private static final String DATABASE_CREATE =
+            "CREATE TABLE if not exists " + SQLITE_TABLE + " (" +
+                    KEY_ROWID + " INTEGER PRIMARY KEY autoincrement," +
+                    KEY_NAME + " TEXT," +
+                    KEY_SCORE + " INTEGER," +
+                    KEY_DIFFICULTY + " TEXT," +
+                    KEY_NUMROWS + " TEXT," +
+                    KEY_NUMCOLUMNS + " TEXT," +
+                    KEY_SPEED + " TEXT);";
+
+    public static void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + SQLITE_TABLE);
+        onCreate(db);
+    }
+
 }
--- app/src/main/java/com/example/quadrominotest/LeaderboardDBHelper.java
+++ app/src/main/java/com/example/quadrominotest/LeaderboardDBHelper.java
@@ -1,4 +1,27 @@
 package com.example.quadrominotest;
 
-public class LeaderboardDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LeaderboardDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "Leaderboard";
+    private static final int DATABASE_VERSION = 1;
+
+    LeaderboardDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        LeaderboardDB.onCreate(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        LeaderboardDB.onUpgrade(db, oldVersion, newVersion);
+    }
+
+
 }
